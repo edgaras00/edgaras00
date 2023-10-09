93a9620671a787d7be05be8da1991b433fa97cd6
edgar <edgaras993@gmail.com>
2023-10-09 17:14:33 -0400

    cleanup

--- index.js
+++ index.js
@@ -48,19 +48,3 @@ window.addEventListener("resize", () => {
     skillSecond.classList.remove("hidden");
   }
 });
-
-function Student() {}
-
-Student.prototype.sayName = function () {
-  console.log(this.name);
-};
-
-function Firstgrader(name) {
-  this.name = name;
-  this.grade = 8;
-}
-
-Firstgrader.prototype = Object.create(Student.prototype);
-
-const s1 = new Firstgrader("Steve");
-s1.sayName();
